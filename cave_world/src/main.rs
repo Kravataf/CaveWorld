2a60936f49986594edd19102776ac30fe3913350
Kravataf <159631603+Kravataf@users.noreply.github.com>
2025-07-01 17:26:08 +0200

    10x10 grid + shitty lightning

--- cave_world/src/main.rs
+++ cave_world/src/main.rs
@@ -1,4 +1,8 @@
+use std::f32::consts::PI;
+
 use bevy::{
+    color::palettes::css::*,
+    pbr::CascadeShadowConfigBuilder,
     prelude::*,
     window::PresentMode,
 };
@@ -28,20 +32,49 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     let texture_handle = asset_server.load("texture.png");
-    
+
     let material_handle = materials.add(StandardMaterial {
         base_color_texture: Some(texture_handle),
         ..Default::default()
     });
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(material_handle),
-        Transform::from_xyz(0., 0., 0.),
-    ));
+    for i in 0..10 {
+        for j in 0..10 {
+            commands.spawn((
+                Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+                MeshMaterial3d(material_handle.clone()),
+                Transform::from_xyz(i as f32, 0.0, j as f32),
+            ));
+        }
+    }
 
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(-5., 5., 5.).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    commands.insert_resource(AmbientLight {
+        color: WHITE.into(),
+        brightness: 1.0,
+        ..default()
+    });
+
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::OVERCAST_DAY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+        .build(),
     ));
 }
